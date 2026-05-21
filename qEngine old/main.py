ab2e614f71696a06c8b2ce0776f3217cfc7c9720
cloud-dev-007 <lateefmikail@gmail.com>
2026-05-21 13:14:13 +0100

    the app and the backend

--- qEngine old/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def index():
-    return {"details" : "Backend is running designed By Demilade"}
--- qcraft/generated_build.sh
+++
@@ -1,3 +0,0 @@
-
-echo "Build runner generation code ..."
-flutter pub run build_runner watch --delete-conflicting-outputs
